ce7251548d81528900692dbc42aee69e71b660fb
rhodar <bakira4@hotmail.fr>
2013-12-15 18:07:44 +0100

    add Mine class

---
+++ src/Mine/Mine.cpp
@@ -0,0 +1,18 @@
+/* 
+ * File:   MIne.cpp
+ * Author: bakira
+ * 
+ * Created on 15 décembre 2013, 18:05
+ */
+
+#include "Mine.h"
+
+MIne::MIne() {
+}
+
+MIne::MIne(const MIne& orig) {
+}
+
+MIne::~MIne() {
+}
+
---
+++ src/Mine/Mine.h
@@ -0,0 +1,21 @@
+/* 
+ * File:   Mine.h
+ * Author: bakira
+ *
+ * Created on 15 décembre 2013, 18:05
+ */
+
+#ifndef MINE_H
+#define	MINE_H
+
+class Mine {
+public:
+    Mine();
+    Mine(const Mine& orig);
+    virtual ~Mine();
+private:
+
+};
+
+#endif	/* MINE_H */
+
